db8621c1a62524c265d31fd6a0939456b5de4750
teeaaspoon <tommypoon36@gmail.com>
2018-07-10 22:41:30 +0000

    added chatty websocket server into repo

---
+++ chatty_ws_server/server.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const WebSocket = require("ws").Server;
+const uuid = require("uuid");
+
+const PORT = 3001;
+
+const server = express()
+    .use(express.static("public"))
+    .listen(PORT, "0.0.0.0", "localhost", () => console.log(`listening on ${PORT}`));
+
+const wss = new WebSocket({ server });
+
+wss.on("connection", ws => {
+    console.log("Client connected");
+
+    ws.on("message", data => {
+        const newMessage = JSON.parse(data);
+        // handles messages
+        if (newMessage.type === "postMessage") {
+            // add uuid to the newMessage
+            newMessage.id = uuid();
+            newMessage.type = "incomingMessage";
+            console.log(`User ${newMessage.username} said ${newMessage.content}`);
+        } else if (newMessage.type === "postNotification") {
+            newMessage.type = "incomingNotification";
+        } else {
+            throw new Error("Unkown event type " + data.type);
+        }
+
+        // broadcast newMessage to all clients
+        wss.clients.forEach(client => {
+            if (client.readyState === 1) {
+                console.log("sending message back");
+                client.send(JSON.stringify(newMessage));
+            }
+        });
+    });
+
+    ws.on("close", () => console.log("Client disconnected"));
+});
