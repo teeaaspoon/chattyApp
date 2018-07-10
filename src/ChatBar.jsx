caa43cf1a0a93885d8b0d721b0f9061cadcc54c8
teeaaspoon <tommypoon36@gmail.com>
2018-07-10 20:40:24 +0000

    modified chatbar to controlled element

--- src/ChatBar.jsx
+++ src/ChatBar.jsx
@@ -1,28 +1,46 @@
 import React, { Component } from "react";
 import { log } from "util";
 
-function ChatBar(props) {
-    function handleKeyPress(event) {
-        if (event.key === "Enter") {
-            props.newMessageFunction(props.currentUser, event.target.value);
-            event.target.value = "";
-        }
+class ChatBar extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            messageContent: ""
+        };
     }
 
-    return (
-        <footer className="chatbar">
-            <input
-                className="chatbar-username"
-                placeholder="Your Name (Optional)"
-                defaultValue={props.currentUser}
-            />
-            <input
-                className="chatbar-message"
-                placeholder="Type a message and hit ENTER"
-                onKeyPress={handleKeyPress}
-            />
-        </footer>
-    );
+    handleChange = event => {
+        this.setState({ messageContent: event.target.value });
+    };
+
+    handleKeyPress = event => {
+        if (event.key === "Enter") {
+            this.props.newMessageFunction(
+                this.props.currentUser,
+                this.state.messageContent
+            );
+            this.setState({ messageContent: "" });
+        }
+    };
+
+    render() {
+        return (
+            <footer className="chatbar">
+                <input
+                    className="chatbar-username"
+                    placeholder="Your Name (Optional)"
+                    defaultValue={this.props.currentUser}
+                />
+                <input
+                    className="chatbar-message"
+                    placeholder="Type a message and hit ENTER"
+                    onKeyPress={this.handleKeyPress}
+                    onChange={this.handleChange}
+                    value={this.state.messageContent}
+                />
+            </footer>
+        );
+    }
 }
 
 export default ChatBar;
